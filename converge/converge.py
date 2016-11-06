238839b35eb1e8ef1c10ae1e147b4a5e8d6069ad
Andrew Boswell <drewboswell@gmail.com>
2016-11-06 18:42:59 +0100

    ADD: added setup.py and pypi/github structure

--- converge/converge.py
+++ converge/converge.py
@@ -1,89 +1,94 @@
+# -*- coding: utf-8 -*-
+
 import configparser
 import os
 import time
 import logging
 from pprint import pprint
-from BaseFunctions import BaseFunctions
-
-statistics = dict()
-statistics['start_time'] = time.time()
-
-config_path = "../tests/resources/etc/converge.ini"
-config = configparser.ConfigParser()
-config.read(config_path)
-
-# set path for nodes
-if "logging_level" in config['DEFAULT']:
-    logging_level = config["DEFAULT"]["logging_level"]
-else:
-    logging_level = "INFO"
-
-logging.basicConfig(level=logging_level)
-
-repository_path = config['DEFAULT']['repository_path']
-
-# set path for nodes
-if "node_path" in config['DEFAULT']:
-    node_path = config["DEFAULT"]["node_path"]
-else:
-    node_path = os.path.join(repository_path, "nodes")
-
-# set path for node_groups
-if "node_group_path" in config['DEFAULT']:
-    node_group_path = config["DEFAULT"]["node_group_path"]
-else:
-    node_group_path = os.path.join(repository_path, "node_groups")
-
-# set path for packages
-if "package_path" in config['DEFAULT']:
-    package_path = config["DEFAULT"]["package_path"]
-else:
-    package_path = os.path.join(repository_path, "packages")
-
-# set path for applications
-if "application_path" in config['DEFAULT']:
-    application_path = config["DEFAULT"]["application_path"]
-else:
-    application_path = os.path.join(repository_path, "applications")
-    
-# set path for hierarchy
-if "hierarchy_path" in config['DEFAULT']:
-    hierarchy_path = config["DEFAULT"]["hierarchy_path"]
-else:
-    hierarchy_path = os.path.join(repository_path, "hierarchy")
-
-# set recursion depth on package resolution
-if "package_recursion_depth_max" in config['DEFAULT']:
-    package_recursion_depth_max = config["DEFAULT"]["package_recursion_depth_max"]
-else:
-    package_recursion_depth_max = 7
+from .BaseFunctions import BaseFunctions
 
 
-cv = BaseFunctions(repository_path=repository_path,
-                   node_path=node_path,
-                   node_group_path=node_group_path,
-                   package_path=package_path,
-                   application_path=application_path,
-                   hierarchy_path=hierarchy_path,
-                   package_recursion_depth_max=package_recursion_depth_max,
-                   logger=logging)
+# add main entry point
+def main():
+    statistics = dict()
+    statistics['start_time'] = time.time()
 
-cv.resolve_nodes()
-cv.resolve_node_groups()
-cv.resolve_packages()
-cv.resolve_applications()
-#cv.resolve_hierarchy()
+    config_path = "../tests/resources/etc/converge.ini"
+    config = configparser.ConfigParser()
+    config.read(config_path)
 
-#pprint(cv.get_non_resolved_configuration()['packages'])
-# statistics calculations
-statistics["end_time"] = time.time()
-statistics["total_time"] = statistics["end_time"] - statistics['start_time']
+    # set path for nodes
+    if "logging_level" in config['DEFAULT']:
+        logging_level = config["DEFAULT"]["logging_level"]
+    else:
+        logging_level = "INFO"
 
-statistics.update(cv.get_statistics())
+    logging.basicConfig(level=logging_level)
 
-logging.info(statistics)
-logging.info("Finished Run in total of '%s' seconds" % statistics["total_time"])
+    repository_path = config['DEFAULT']['repository_path']
 
-# Todo: add the ability to do AND(/OR?) operations on node_group overrides?
-# Todo: add the ability to extend a pre-existing package (import keys etc)
-# Todo: allow inception in package values (if you write take value from another key ${::}?)
+    # set path for nodes
+    if "node_path" in config['DEFAULT']:
+        node_path = config["DEFAULT"]["node_path"]
+    else:
+        node_path = os.path.join(repository_path, "nodes")
+
+    # set path for node_groups
+    if "node_group_path" in config['DEFAULT']:
+        node_group_path = config["DEFAULT"]["node_group_path"]
+    else:
+        node_group_path = os.path.join(repository_path, "node_groups")
+
+    # set path for packages
+    if "package_path" in config['DEFAULT']:
+        package_path = config["DEFAULT"]["package_path"]
+    else:
+        package_path = os.path.join(repository_path, "packages")
+
+    # set path for applications
+    if "application_path" in config['DEFAULT']:
+        application_path = config["DEFAULT"]["application_path"]
+    else:
+        application_path = os.path.join(repository_path, "applications")
+
+    # set path for hierarchy
+    if "hierarchy_path" in config['DEFAULT']:
+        hierarchy_path = config["DEFAULT"]["hierarchy_path"]
+    else:
+        hierarchy_path = os.path.join(repository_path, "hierarchy")
+
+    # set recursion depth on package resolution
+    if "package_recursion_depth_max" in config['DEFAULT']:
+        package_recursion_depth_max = config["DEFAULT"]["package_recursion_depth_max"]
+    else:
+        package_recursion_depth_max = 7
+
+
+    cv = BaseFunctions(repository_path=repository_path,
+                       node_path=node_path,
+                       node_group_path=node_group_path,
+                       package_path=package_path,
+                       application_path=application_path,
+                       hierarchy_path=hierarchy_path,
+                       package_recursion_depth_max=package_recursion_depth_max,
+                       logger=logging)
+
+    cv.resolve_nodes()
+    cv.resolve_node_groups()
+    cv.resolve_packages()
+    cv.resolve_applications()
+    #cv.resolve_hierarchy()
+
+    #pprint(cv.get_non_resolved_configuration()['packages'])
+    # statistics calculations
+    statistics["end_time"] = time.time()
+    statistics["total_time"] = statistics["end_time"] - statistics['start_time']
+
+    statistics.update(cv.get_statistics())
+
+    logging.info(statistics)
+    logging.info("Finished Run in total of '%s' seconds" % statistics["total_time"])
+
+    # Todo: add the ability to do AND(/OR?) operations on node_group overrides?
+    # Todo: add the ability to extend a pre-existing package (import keys etc)
+    # Todo: allow inception in package values (if you write take value from another key ${::}?)
---
+++ setup.py
@@ -0,0 +1,57 @@
+# -*- coding: utf-8 -*-
+
+
+"""setup.py: setuptools control."""
+
+import re
+from setuptools import setup, find_packages
+import io
+import codecs
+import os
+import sys
+
+# here = os.path.abspath(os.path.dirname(__file__))
+
+version = re.search(
+    '^__version__\s*=\s*"(.*)"',
+    open('converge/__init__.py').read(),
+    re.M
+).group(1)
+
+with open("README.md", "rb") as f:
+    long_descr = f.read().decode("utf-8")
+
+setup(
+    name="converge",
+    packages=["converge"],
+    entry_points={
+        "console_scripts": ['converge = converge.converge:main']
+    },
+    license='Apache License 2.0',
+    version=version,
+    install_requires=['yaml'],
+    description="Python command line application bare bones template.",
+    long_description=long_descr,
+    author="Andrew Boswell",
+    author_email="drewboswell@gmail.com",
+    url="https://github.com/drewboswell/converge",
+    platforms='any',
+    classifiers=[
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'Intended Audience :: DevOps Engineers',
+        'Intended Audience :: Financial and Insurance Industry',
+        'Intended Audience :: IT Operations',
+        'Operating System :: OS Independent',
+        'Topic :: Office/Business',
+        'Topic :: Utilities :: Configuration Management',
+        'Topic :: System :: Configuration Management',
+        'Topic :: Software Development Configuration Management',
+        'License :: OSI Approved :: Apache License 2.0'
+    ],
+    keywords='configuration management development operations system sysadmin config converge'
+)
