8e442ff9888f3f936d9960ad2b7c7339e182726f
Andrew Boswell <drewboswell@gmail.com>
2016-11-26 17:14:50 +0100

    Start fresh, kanban oriented

--- converge/__main__.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-
-
-"""converge.__main__: executed when converge directory is called as script."""
-
-from .converge import main
-main()
--- test/test_converge.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-import sys
-
-sys.path.insert(0, os.path.abspath('..'))
-from converge.BaseFunctions import BaseFunctions
-
-
-# this is just an example test file (don't like the sys.path append though)
