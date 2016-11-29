8bbfc82ee5af8fd4168b51d0ec8019b4a7cafa9b
Andrew Boswell <drewboswell@gmail.com>
2016-11-29 10:12:11 +0100

    updated tests

--- tests/test_converge.py
+++ tests/test_converge.py
@@ -2,10 +2,10 @@ import pytest
 import sys, os
 
 sys.path.append(os.path.abspath(os.path.join('..', 'converge')))
-import converge.converge
+import converge.converge as converge
 
 
 class TestConverge:
     def test_main_arg_parser(self):
-        result = converge.converge.main()
+        result = converge.main()
         assert result is True
