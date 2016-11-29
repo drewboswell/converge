d78858abb6d211e631d601c7d176eedb130fecd6
Andrew Boswell <drewboswell@gmail.com>
2016-11-29 09:50:21 +0100

    added argument parser with options and converge running for project

---
+++ converge-runner.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3.5
+# -*- coding: utf-8 -*-
+
+
+"""Convenience wrapper for running converge directly from source tree."""
+
+from converge.converge import main
+
+if __name__ == '__main__':
+    main()
