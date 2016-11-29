b8cf718a9c681d84e8084508c5c869695c942552
Andrew Boswell <drewboswell@gmail.com>
2016-11-29 10:13:45 +0100

    added test for bad argument passing

--- tests/test_converge.py
+++ tests/test_converge.py
@@ -9,3 +9,8 @@ class TestConverge:
     def test_main_arg_parser(self):
         result = converge.main()
         assert result is True
+
+    def test_main_arg_parser_bad_argument(self):
+        sys.argv.append("break arguments")
+        result = converge.main()
+        assert result is False
