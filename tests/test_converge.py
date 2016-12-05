890795be5d25d66243aeb5b5602c30b856671f94
Andrew Boswell <drewboswell@gmail.com>
2016-12-05 19:29:04 +0100

    test package executability

--- tests/test_converge.py
+++ tests/test_converge.py
@@ -2,8 +2,8 @@ import pytest
 import sys, os
 
 sys.path.append(os.path.abspath(os.path.join('..', 'converge')))
-import converge.converge as converge
-
+from converge import converge as converge
+import converge.__main__ as mainexec
 
 class TestConverge:
     def test_main_arg_parser(self):
@@ -13,4 +13,9 @@ class TestConverge:
     def test_main_arg_parser_bad_argument(self):
         sys.argv.append("break arguments")
         result = converge.main()
+        sys.argv.remove("break arguments")
         assert result is False
+
+    def test_package_executable_main(self):
+        result = mainexec.main()
+        assert result is True
