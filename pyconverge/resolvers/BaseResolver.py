158bcfc6609a2d2a70c708dfe80befe1edd5b31c
Andrew Boswell <drewboswell@gmail.com>
2017-01-08 23:57:27 +0100

    periodic commit

--- pyconverge/resolvers/BaseResolver.py
+++
@@ -1,8 +0,0 @@
-from abc import ABC, abstractmethod
-
-
-class BaseResolver(ABC):
-
-    @abstractmethod
-    def resolve_data(self, unresolved_data):
-        pass
--- pyconverge/writers/BaseWriter.py
+++
@@ -1,8 +0,0 @@
-from abc import ABC, abstractmethod
-
-
-class BaseWriter(ABC):
-
-    @abstractmethod
-    def write_data(self, resolved_data):
-        pass
