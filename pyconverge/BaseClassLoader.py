0695cfe6a1d2029fefc27365e01938103f35b828
Andrew Boswell <drewboswell@gmail.com>
2017-01-10 23:00:42 +0100

    added possibility for new yaml based conf file

--- pyconverge/BaseClassLoader.py
+++ pyconverge/BaseClassLoader.py
@@ -36,11 +36,23 @@ class BaseClassLoader:
             plugin_impl.write_data(resolved_data=resolved_data)
         return loaded_data
 
+    def run_validators(self):
+        validation_data = dict()
+        for validator in self.dynamic_classes["validators"]:
+            log.info("Validation Class %s initializing" % validator)
+            validation = validator.validate()
+            validation_data[validator] = validation
+        return validation_data
+
     def run_readers(self):
         loaded_data = dict()
         for reader in self.dynamic_classes["readers"]:
             log.info("Reader Class %s initializing" % reader)
-            loaded_data[reader] = reader.read_data()
+            read_data = reader.read_data()
+            for read_filter in self.dynamic_classes["read_filters"]:
+                log.info("Post Read Filter Class %s initializing" % read_filter)
+                read_data = read_filter.filter_data(read_data=read_data)
+            loaded_data[reader] = read_data
         return loaded_data
 
     def run_resolvers(self, unresolved_data):
@@ -53,5 +65,8 @@ class BaseClassLoader:
     def run_writers(self, resolved_data):
         written_data = dict()
         for writer in self.dynamic_classes["readers"]:
+            for write_filter in self.dynamic_classes["write_filters"]:
+                log.info("Pre Write Filter Class %s initializing" % write_filter)
+                resolved_data = write_filter.filter_data(resolved_data=resolved_data)
             log.info("Writer Class %s initializing" % writer)
             written_data[writer] = writer.write_data(resolved_data=resolved_data)
